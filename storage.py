02fbbba699a86b0510b0311747a3f21c263ff771
Frankosmel <130179322+Frankosmel@users.noreply.github.com>
2025-07-22 16:25:40 -0400

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,26 +1,23 @@
 import json
-import os
 from config import FILES
 
 def ensure_files():
-    """
-    Crea todos los JSON (vacíos) si no existen.
-    """
+    """Crea todos los JSON vacíos si no existen."""
     for path in FILES.values():
-        if not os.path.exists(path):
+        try:
+            open(path, 'r').close()
+        except FileNotFoundError:
             with open(path, 'w') as f:
                 json.dump({}, f)
 
-def load(file_key):
-    """
-    Carga y devuelve el contenido del JSON indicado por la clave.
-    """
-    with open(FILES[file_key], 'r') as f:
+def load(key: str):
+    """Carga el JSON asociado a FILES[key]."""
+    path = FILES[key]
+    with open(path, 'r') as f:
         return json.load(f)
 
-def save(file_key, data):
-    """
-    Guarda el diccionario `data` en el JSON indicado por la clave.
-    """
-    with open(FILES[file_key], 'w') as f:
+def save(key: str, data):
+    """Guarda `data` en el JSON asociado a FILES[key]."""
+    path = FILES[key]
+    with open(path, 'w') as f:
         json.dump(data, f, indent=2)
