89008d9af77287de28414451f58a0265b72c00bb
Frankosmel <130179322+Frankosmel@users.noreply.github.com>
2025-07-22 23:29:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from auth import is_valid
 from admin_handlers import register_admin_handlers, show_admin_menu
 from owner_handlers import register_owner_handlers, show_owner_menu
 from raffle_handlers import register_referral_handlers, register_raffle_handlers
-from draw_handlers import register_draw_handlers
+from draw_handlers import do_draw             # IMPORTAMOS do_draw, no register_draw_handlers
 from scheduler import load_jobs, start_reminders
 from payments_handlers import register_payment_handlers
 
@@ -54,7 +54,7 @@ register_referral_handlers(bot)
 register_raffle_handlers(bot)
 register_admin_handlers(bot)
 register_owner_handlers(bot)
-register_draw_handlers(bot)
+do_draw(bot)                        # Aquí arrancamos el módulo de draw_handlers
 register_payment_handlers(bot)
 load_jobs(bot)
 start_reminders(bot)
